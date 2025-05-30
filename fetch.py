3708108564964f3c5f9a5aa69c85f66dabeeb826
anhvlt-2k6 <anhvlt.2006@outlook.com>
2025-05-30 11:34:10 +0700

    update the lab1 and fetch py

--- fetch.py
+++ fetch.py
@@ -17,7 +17,11 @@ htm_header = """
 <body>
   <h1>WED201c Repository for submission</h1>
   <p>
-  Below are the list of deployed/designed website that maybe use for submission.
+    Below are the list of deployed/designed website that maybe use for
+    submission.
+    <br />
+    View the source code on <a href="https://github.com/anhvlt-2k6/WED201c_deploys" target="_blank">GitHub</a>.
+    <br />
   </p>
   <ul>
 """
@@ -33,5 +37,5 @@ with open("index.html", "w") as index_file:
     for htm_file in find_htm_files(path):
         if "index.html" not in htm_file:
             htm_link = os.path.relpath(htm_file, path)
-            index_file.write('\n    <li><a href="{htm_link}">{htm_link}</a></li>'.format(htm_link=htm_link))
+            index_file.write('\n    <li>\n      <a href="{htm_link}">{htm_link}</a>\n    </li>'.format(htm_link=htm_link))
     index_file.write(htm_footer)
